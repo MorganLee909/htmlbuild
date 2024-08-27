a32a525f32a71f98c746ca5a98c4ae975ff07f3e
Lee Morgan <lee@leemorgan.dev>
2024-08-27 19:35:19 -0400

    Serve index.html files for each route. Add compression.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const esbuild = require("esbuild");
+const compression = require("compression");
 const fs = require("node:fs/promises");
 
 const findRoutes = async (dir)=>{
@@ -15,10 +16,12 @@ const findRoutes = async (dir)=>{
 
 const htmlbuild = async (options)=>{
     const app = express();
+    app.use(compression());
+
     const routes = await findRoutes(`${__dirname}/routes`);
 
     for(let i = 0; i < routes.length; i++){
-        app.get(routes[i].replace(`${__dirname}/routes`, ""), (req, res)=>{res.send(routes[i])});
+        app.get(routes[i].replace(`${__dirname}/routes`, ""), (req, res)=>{res.sendFile(`${routes[i]}/index.html`)});
     }
 
     app.listen(8000);
