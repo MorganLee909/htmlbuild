91ed7b1d2dc7ecad93471bc970f516df401830d0
Lee Morgan <lee@leemorgan.dev>
2024-08-27 20:14:49 -0400

    Working on including components.

--- index.js
+++ index.js
@@ -14,17 +14,51 @@ const findRoutes = async (dir)=>{
     return routes;
 }
 
+const buildBundle = async (html)=>{
+    const file = await fs.readFile(`${html}/index.html`, "utf8");
+    let newFile = "";
+
+    for(let i = 0; i < file.length; i++){
+        if(file[i] === "<" && file[i+1].charCodeAt(0) >= 65 && file[i+1].charCodeAt(0) <= 90){
+            let j = 1;
+            while(file[i+j] !== ">"){
+                j++;
+            }
+            let tag = file.substring(file[i], file[i+j]);
+            tag = tag.replaceAll("<", "");
+            tag = tag.replaceAll(">", "");
+            tag = tag.replaceAll("/", "");
+            tag = tag.replaceAll("-", "");
+            newFile += await fs.readFile(`${html}/${tag}.html`);
+            
+            i += j;
+        }else{
+            newFile += file[i];
+        }
+    }
+
+    //Working here
+    //Need to create direction if it doesn't already exist
+    console.log(html.replace(`${__dirname}/routes`, ""));
+    let createdFile = `${__dirname}/build/${html.replace(`${__dirname}/routes`, "")}index.html`;
+    console.log(createdFile);
+    await fs.writeFile(createdFile, newFile);
+    return createdFile;
+}
+
 const htmlbuild = async (options)=>{
     const app = express();
     app.use(compression());
 
     const routes = await findRoutes(`${__dirname}/routes`);
-
     for(let i = 0; i < routes.length; i++){
-        app.get(routes[i].replace(`${__dirname}/routes`, ""), (req, res)=>{res.sendFile(`${routes[i]}/index.html`)});
+        const routeFile = await buildBundle(routes[i]);
+        app.get(routes[i].replace(`${__dirname}/routes`, ""), (req, res)=>{res.sendFile(routeFile)});
     }
 
     app.listen(8000);
 }
 
 htmlbuild();
+
+module.exports = htmlbuild();
